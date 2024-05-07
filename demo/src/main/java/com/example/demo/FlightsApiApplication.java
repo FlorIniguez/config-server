7760eefd91e3adab616f626a3bd2034aa0fe5f4d
FlorIniguez <iniguezflorencia788@gmail.com>
2024-05-06 23:16:18 -0300

    arreglando flights

--- demo/src/main/java/com/example/demo/FlightsApiApplication.java
+++ demo/src/main/java/com/example/demo/FlightsApiApplication.java
@@ -8,8 +8,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //notacion para registar servicio
 @EnableDiscoveryClient
 public class FlightsApiApplication {
+
     public static void main(String[] args) {
+
         SpringApplication.run(FlightsApiApplication.class, args);
+
     }
 
 
